9c76ffa349103f363e5d277f37cd1f502858a495
Parth2031 <rastogiparth2011@gmail.com>
2021-12-17 18:39:47 +0530

    Question Solved

---
+++ Basic/InverseANumber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int InversedNumber(int n)
+{
+
+}
+
+int main(int args, char** argv)
+{
+  int n;
+  cout<<endl<<"Enter a Number: ";
+  cin>>n;
+  cout<<"Inversed Number: ";
+  InversedNumber(n);
+  return 0;
+}
